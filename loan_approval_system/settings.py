64bb6186e722dfdd510e550fb724c9a17da413b6
jimnaught <164331990+jimnaught@users.noreply.github.com>
2024-03-21 19:58:15 +0200

    Update settings.py

--- loan_approval_system/settings.py
+++ loan_approval_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!cw@7kz%x-q*olz+o6i=m80mvl=bs8z1%+r*c*v0tz6*_x2sb-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
